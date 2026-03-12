4d3e7ecc1fc383a04f4ffd415777fac5e16ef0e1
Lionnel Eng <contact@mistral-it.fr>
2026-03-12 13:03:11 +0100

    fix: change page module

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Navbar from "@/components/Navbar";
-import HeroSection from "@/components/HeroSection";
-import AuditsSection from "@/components/AuditsSection";
-import ServicesSection from "@/components/ServicesSection";
-import ProcessSection from "@/components/ProcessSection";
-import WhySection from "@/components/WhySection";
-import ContactSection from "@/components/ContactSection";
-import Footer from "@/components/Footer";
-import ScrollReveal from "@/components/ScrollReveal";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import AuditsSection from "../components/AuditsSection";
+import ServicesSection from "../components/ServicesSection";
+import ProcessSection from "../components/ProcessSection";
+import WhySection from "../components/WhySection";
+import ContactSection from "../components/ContactSection";
+import Footer from "../components/Footer";
+import ScrollReveal from "../components/ScrollReveal";
 
 export default function Home() {
   return (

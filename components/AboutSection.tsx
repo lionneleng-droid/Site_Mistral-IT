00aec33c5d50a0b1351258b485e91116d9b3ee3f
Lionnel Eng <contact@mistral-it.fr>
2026-03-13 11:21:38 +0100

    fix: taille photo hauteur

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -10,7 +10,7 @@ export default function AboutSection() {
             <Image
               src="/images/ENG-Lionnel.jpg" // Remplacez par le chemin de votre photo
               alt="Lionnel ENG"
-              width={300}
+              width={500}
               height={400}
               className="rounded-full object-cover"
             />

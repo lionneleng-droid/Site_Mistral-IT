f827d189308fc5c23822d8e0d49bee720506fcca
Lionnel Eng <contact@mistral-it.fr>
2026-03-13 11:11:11 +0100

    fix: name photo

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -8,7 +8,7 @@ export default function AboutSection() {
         <div className="max-w-4xl mx-auto flex flex-col md:flex-row items-center gap-8">
           <div className="flex-shrink-0">
             <Image
-              src="/images/ENG Lionnel.png" // Remplacez par le chemin de votre photo
+              src="/images/ENG-Lionnel.png" // Remplacez par le chemin de votre photo
               alt="Lionnel ENG"
               width={300}
               height={300}

6156cf944656469d60512aee242aba9f0ca0b1fc
lionnel.eng <lionnel.eng@homeserve.fr>
2026-04-03 23:20:38 +0200

    fix: ménage dashboard

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuditStatsDashboard from '@/components/AuditStatsDashboard';
-
-export default function DashboardPage() {
-  return (
-    <div className="min-h-screen bg-stone">
-      <AuditStatsDashboard />
-    </div>
-  );
-}
